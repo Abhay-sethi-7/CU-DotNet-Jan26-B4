8431b046f7d143f6173819a8e68800f50e2c184b
Abhay Kumar Sethi <Sethi.abhay.7876@gmail.com>
2026-01-14 15:53:23 +0530

    day 8 done

---
+++ Daily Assignments/Day 8/LoginMessageProcessor/LoginMessageProcessor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LoginMessageProcessor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] parts = input.Split('|');
+
+            string userName = parts[0];
+            string loginMessage = parts[1];
+
+            string cleanedMessage = loginMessage.Trim().ToLower();
+
+            string standardMessage = "login successful";
+
+            bool containsSuccessful = cleanedMessage.Contains("successful");
+
+            string status;
+
+            if (!containsSuccessful)
+            {
+                status = "LOGIN FAILED";
+            }
+            else if (cleanedMessage.Equals(standardMessage))
+            {
+                status = "LOGIN SUCCESS";
+            }
+            else
+            {
+                status = "LOGIN SUCCESS (wellcome!)";
+            }
+
+            Console.WriteLine($"Status  : {status}");
+        }
+    }
+}
+
