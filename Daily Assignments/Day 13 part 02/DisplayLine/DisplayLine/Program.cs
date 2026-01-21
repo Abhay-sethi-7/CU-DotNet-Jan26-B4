3850f66040199ba421dbd2f9ba5bf9d328995704
Abhay Kumar Sethi <Sethi.abhay.7876@gmail.com>
2026-01-21 14:57:02 +0530

    day 13

---
+++ Daily Assignments/Day 13 part 02/DisplayLine/DisplayLine/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DisplayLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayLine();
+            DisplayLine('+');
+            DisplayLine('$', 60);
+
+        }
+        static void DisplayLine(char ch = '-', int count = 40)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Daily Assignments/Day 13 part 1/GymFees/GymFees/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace GymFees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateGymMembership(true, false, true));
+
+        }
+
+        static double CalculateGymMembership(bool treadmill, bool weightLifting, bool zumba)
+        {
+            double amount = 1000;
+            bool serviceSelected = false;
+
+            if (treadmill)
+            {
+                amount += 300;
+                serviceSelected = true;
+            }
+
+            if (weightLifting)
+            {
+                amount += 500;
+                serviceSelected = true;
+            }
+
+            if (zumba)
+            {
+                amount += 250;
+                serviceSelected = true;
+            }
+
+            if (!serviceSelected)
+                amount += 200;
+
+            amount += amount * 0.05;
+            return amount;
+        }
+
+    }
+}
