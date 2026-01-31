c5d0e36bf6dfde7e366b4ed0574da6336183775f
Abhay Kumar Sethi <Sethi.abhay.7876@gmail.com>
2026-01-31 12:28:28 +0530

    week 4 assignment + day 22

---
+++ Assessment/week4Assesment/HospitalBillingEngine/HospitalBillingEngine/Program.cs
@@ -0,0 +1,138 @@
+﻿namespace HospitalBillingEngine
+{
+    class Patient
+    {
+        public string Name { get; set; }
+        public decimal BaseFees { get; set; }
+        public Patient(string name, decimal baseFees)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                Name = "Unknown";   
+            else
+                Name = name;
+
+            if (baseFees < 0)
+                BaseFees = 0;       
+            else
+                BaseFees = baseFees;
+        }
+        public virtual decimal CalculateFinalBill()
+        {
+            return BaseFees;
+        }
+    }
+    class Inpatient : Patient
+    {
+        public int DaysStayed { get; set; }
+        public decimal DailyRate { get; set; }
+
+        public Inpatient(string name, decimal baseFee, int daysStayed, decimal dailyRate)
+            : base(name, baseFee)
+        {
+            DaysStayed = daysStayed;
+            DailyRate = dailyRate;
+        }
+        public override decimal CalculateFinalBill()
+        {
+            return BaseFees + (DaysStayed * DailyRate);
+        }
+    }
+    class Outpatient : Patient
+    {
+        public decimal ProcedureFee { get; set; }
+
+        public Outpatient(string name, decimal baseFee, decimal procedureFee)
+            : base(name, baseFee)
+        {
+            ProcedureFee = procedureFee;
+        }
+
+        public override decimal CalculateFinalBill()
+        {
+            return BaseFees + ProcedureFee;
+        }
+    }
+    class EmergencyPatient : Patient
+    {
+        private int severityLevel; 
+
+        public int SeverityLevel
+        {
+            get { return severityLevel; }
+            set
+            {
+                if (value < 1) severityLevel = 1;
+                else if (value > 5) severityLevel = 5; 
+                else severityLevel = value;
+            } }
+
+        public EmergencyPatient(string name, decimal baseFee, int severityLevel)
+            : base(name, baseFee)
+        {
+            SeverityLevel = severityLevel;
+        }
+
+        public override decimal CalculateFinalBill()
+        {
+            return BaseFees * SeverityLevel;
+        }
+    }
+
+    class HospitalBilling
+    {
+        private List<Patient> patients = new List<Patient>();
+
+        public void AddPatient(Patient p)
+        {
+            patients.Add(p);
+        }
+
+        public void GenerateDailyReport()
+        {
+            Console.WriteLine("----- Daily Report -----");
+            foreach (var p in patients)
+            {
+                Console.WriteLine($"{p.Name} : {p.CalculateFinalBill():C2}");
+            }
+        }
+
+        public decimal CalculateTotalRevenue()
+        {
+            decimal total = 0;
+            foreach (var p in patients)
+            {
+                total += p.CalculateFinalBill();
+            }
+            return total;
+        }
+
+        public int GetInpatientCount()
+        {
+            int count = 0;
+            foreach (var p in patients)
+            {
+                if (p is Inpatient) count++;
+            }
+            return count;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HospitalBilling billing = new HospitalBilling();
+
+            billing.AddPatient(new Inpatient("Abhay", 500, 3, 200));
+            billing.AddPatient(new Outpatient("Rahul", 300, 150));
+            billing.AddPatient(new EmergencyPatient("Nahida", 400, 4));
+
+            billing.GenerateDailyReport();
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Total Revenue: " + billing.CalculateTotalRevenue().ToString("C2"));
+
+            Console.WriteLine("Number of Inpatients: " + billing.GetInpatientCount());
+        }
+    }
+}
---
+++ Daily Assignments/Day 22/OLA/OLA/Program.cs
@@ -0,0 +1,130 @@
+﻿namespace OLA
+{
+    class Ride
+    {
+        public int RideID { get; set; }
+        public double Fare { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+
+    class OLADriver
+    {
+        public int DriverID { get; set; }
+        public string DriverName { get; set; }
+        public string VehicleNumber { get; set; }
+
+        public List<Ride> Rides { get; set; }
+
+
+        public OLADriver()
+        {
+            Rides = new List<Ride>();
+        }
+
+        public void DisplayAllRides()
+        {
+            if (Rides.Count == 0)
+            {
+                Console.WriteLine("No rides found.");
+                return;
+            }
+            double TotalFare = 0;
+
+            foreach (Ride r in Rides)
+            {
+                Console.WriteLine(
+                    $"RideID: {r.RideID}, From: {r.From}, To: {r.To}, Fare: {r.Fare}"
+                );
+                TotalFare += r.Fare;
+
+            }
+            Console.WriteLine($"Total fare : {TotalFare}");
+        }
+        public override string ToString()
+        {
+            return $"Driver Name- {DriverName} Driver Id- {DriverID} Driver Vehicle Number = {VehicleNumber}";
+
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //OLADriver d1 = new OLADriver()
+            //{
+            //    DriverID = 1,
+            //    DriverName = "x",
+            //    VehicleNumber = "HP-22-AB-1234"
+            //};
+            //d1.Rides.Add(new Ride()
+            //{
+            //    RideID = 893,
+            //    Fare = 290.0,
+            //    From = "Chd",
+            //    To = "Mohali"
+            //});
+            //d1.Rides.Add(new Ride()
+            //{
+            //    RideID = 245,
+            //    Fare = 451.0,
+            //    From = "delhi",
+            //    To = "gurugram"
+            //});
+            //Console.WriteLine(d1);
+            //d1.DisplayAllRides();
+            List<OLADriver> drivers = new List<OLADriver>
+            {
+                new OLADriver
+                {
+                    DriverID = 1,
+                    DriverName = "Ab",
+                    VehicleNumber = "HP-01-AB-1234",
+                    Rides = new List<Ride>
+                    {
+                        new Ride { RideID = 101, From = "Chd", To = "Mohali", Fare = 250 },
+                        new Ride { RideID = 102, From = "Mohali", To = "Kharar", Fare = 180 }
+                    }
+                },
+                new OLADriver
+                {
+                    DriverID = 2,
+                    DriverName = "Rd",
+                    VehicleNumber = "HP-02-CD-5678",
+                    Rides = new List<Ride>
+                    {
+                        new Ride { RideID = 201, From = "Delhi", To = "Gurugram", Fare = 400 },
+                        new Ride { RideID = 202, From = "Gurugram", To = "Noida", Fare = 350 },
+                        new Ride { RideID = 203, From = "Noida", To = "Delhi", Fare = 300 }
+                    }
+                },
+                new OLADriver
+                {
+                    DriverID = 3,
+                    DriverName = "Sa",
+                    VehicleNumber = "HP-03-EF-9012"
+
+                }
+            };
+
+
+            foreach (var driver in drivers)
+            {
+                Console.WriteLine(driver);
+                Console.WriteLine();
+                driver.DisplayAllRides();
+                Console.WriteLine("----------------------------------------");
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
