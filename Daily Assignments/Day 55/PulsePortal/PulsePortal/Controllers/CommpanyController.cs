c221a6e0ead095f43f128c3d1a93ec4ce206cd59
Abhay Kumar Sethi <Sethi.abhay.7876@gmail.com>
2026-03-13 10:33:36 +0530

    Day 55

---
+++ Daily Assignments/Day 55/PulsePortal/PulsePortal/Controllers/CommpanyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PulsePortal.Models;
+
+namespace PulsePortal.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            List<Employee> employees = new List<Employee>()
+        {
+            new Employee{ Id = 1, Name="A", Position="Developer", Salary=50000 },
+            new Employee{ Id = 2, Name="B", Position="Designer", Salary=45000 },
+            new Employee{ Id = 3, Name="C", Position="Manager", Salary=65000 },
+            new Employee{ Id = 4, Name="D", Position="Developer", Salary=55000 }
+        };
+
+            ViewBag.Announcement = "Team meeting at 2PM!!";
+
+            ViewData["DepartmentName"] = "Software Development";
+            ViewData["IsActive"] = true;
+
+            return View(employees);
+        }
+    }
+}
---
+++ Daily Assignments/Day 55/PulsePortal/PulsePortal/Models/Emp.cs
@@ -0,0 +1,12 @@
+﻿namespace PulsePortal.Models
+{
+   
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Position { get; set; }
+            public decimal Salary { get; set; }
+        }
+    
+}
